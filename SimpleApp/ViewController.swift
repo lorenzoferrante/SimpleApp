b50ec2a1a3c9f0afdea1fb5fc38c5267dee1f548
Lorenzo Ferrante <lorenzoferrante96@gmail.com>
2020-04-15 13:23:45 +0200

    change title

--- SimpleApp/ViewController.swift
+++ SimpleApp/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        self.title = "Hello"
+        self.title = "WorkingCopy + XBuilder = XCode on iPad!"
     }
 
 
